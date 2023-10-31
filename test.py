e8bc1a8d96742348ab0d1599753d56c926353ef4
Farhan Zaman <farhanzaman1895@gmail.com>
2023-10-31 13:18:22 -0600

    OOP test

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-
 def test():
     print("calling a funciton test")
 
@@ -7,3 +6,10 @@ print("running the test file")
 if __name__ == "__main__":
     test()
     print("running the test file")
+
+class Test:
+    def __init__(self) -> None:
+        pass
+
+    def prints(self, num: int):
+        print("%d" % num)
