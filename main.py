583325eada43e6964d22cb73140e1e66a93e7f0c
Jason Wang <j.wang73@share.espb.ca>
2023-10-31 13:08:44 -0600

    test

---
+++ main.py
@@ -0,0 +1,8 @@
+###---YEAR 1 PROJECT 1---###
+
+from test import test
+
+print("this is the main program")
+
+if __name__ == "__main__":
+    test()
--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print("this is a test")
-#insert gibberish
+
+def test():
+    print("calling a funciton test")
+
+print("running the test file")
+
+if __name__ == "__main__":
+    test()
+    print("running the test file")
